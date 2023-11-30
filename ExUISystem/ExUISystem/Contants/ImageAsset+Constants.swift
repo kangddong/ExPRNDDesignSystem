5376253f59cafcc2d21982a4a1d048a16bf98d00
kangddong <rkdehddud96@naver.com>
2023-12-01 00:38:44 +0900

    icInfoInfomation add

--- ExUISystem/ExUISystem/Contants/ImageAsset+Constants.swift
+++ ExUISystem/ExUISystem/Contants/ImageAsset+Constants.swift
@@ -10,4 +10,5 @@ import Foundation
 extension ImageAsset {
     static let sampleImage = ImageAsset("sample_image", in: .designSystem, format: .image)
     static let sampleAnimatedImage = ImageAsset("sample_gif", in: .designSystem, format: .gif)
+    static let icInfoInfomation = ImageAsset("info.circle.fill", in: .main, format: .image)
 }
